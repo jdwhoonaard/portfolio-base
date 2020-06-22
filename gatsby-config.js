ff900c1fd51b141d139d2745b602fa35f5cbf049
Job van den Hoonaard <jobvdhoonaard@gmail.com>
2020-06-22 10:52:46 +0200

    changed contentful repository

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,7 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `16d0dg2euvdm`,
+        spaceId: `usgt0m284301`,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
         downloadLocal: true,
       },
