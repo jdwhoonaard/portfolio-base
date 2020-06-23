c5a520379253b8daf9c1670d958046dc5efba4d7
Job van den Hoonaard <jobvdhoonaard@gmail.com>
2020-06-23 10:24:21 +0200

    updated image component

--- src/components/contentful/imageGroup/index.js
+++ src/components/contentful/imageGroup/index.js
@@ -8,18 +8,29 @@ const ImageGroup = ({ render }) => {
   if (render.images.length === 1) {
     return (
       <div className="grid__row fixed contentful__imageGroup">
-        <div className="sm-4 md-offset-1 md-6 lg-offset-1 lg-10 xl-offset-1 xl-10" >
+        <div className="sm-4 md-offset-1 md-6 lg-12 xl-12" >
           {render.images.map(image => (
             <Img fluid={image.fluid} />
           ))}
         </div>
       </div>
     )
-  } else if (render.images.length !== 1) {
+  } else if (render.images.length !== 1 && !(render.images.length % 2)) {
     return (
       <div className="grid__row fixed contentful__imageGroup">
         {render.images.map((image, i) => (
-          <div className={`sm-4 md-offset-1 md-6 lg-5 xl-5 ${i % 2 ? '' : 'lg-offset-1 xl-offset-1'}`} >
+          <div className={`sm-4 md-offset-1 md-6 lg-6 xl-6`} >
+            <Img fluid={image.fluid} />
+          </div>
+        ))
+        }
+      </div >
+    )
+  } else if (render.images.length !== 1 && render.images.length % 2) {
+    return (
+      <div className="grid__row fixed contentful__imageGroup">
+        {render.images.map((image, i) => (
+          <div className={`sm-4 md-offset-1 md-6 lg-4 xl-4`} >
             <Img fluid={image.fluid} />
           </div>
         ))
